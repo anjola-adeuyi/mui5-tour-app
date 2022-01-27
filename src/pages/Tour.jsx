acb5abe88c3d9185387470aac45310a601de7fe9
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-01-27 23:59:34 +0100

    Accordion and Modal added

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -1,6 +1,8 @@
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Container, Typography, BottomNavigation, Paper } from '@mui/material'
 import React from 'react';
 import ImageCollage from '../components/ImageCollage'
+import TourAccordion from '../components/TourAccordion'
+import BasicModal from '../components/TourModal'
 
 const Tour = () => {
   return <Container sx={{ width: 900 }} >
@@ -15,7 +17,26 @@ const Tour = () => {
     <Typography variant="h6" component="h4" marginTop={3}>
       About this ticket
     </Typography>
+    <Typography variant="paragraph" component="p" marginTop={3}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi numquam ad, eos, in quis incidunt eligendi laboriosam, quam fugit adipisci aperiam sed quo! Harum adipisci ipsa velit, ad iste id. Explicabo enim quisquam maiores molestias esse odit et debitis ipsa!
+    </Typography>
     </Box>
+
+    <Box marginBottom={10}>
+      <Typography variant="h6" component="h4" marginTop={3}>
+        Frequently asked questions
+      </Typography>
+      <TourAccordion />
+    </Box>
+  
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <BottomNavigation
+        showLabels
+      >
+        <BasicModal />
+      </BottomNavigation>
+    </Paper>
+
   </Container>;
 };
 
