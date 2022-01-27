dbdd463f9d718586af56ae71fe058736bb2b9e6c
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-01-27 23:16:24 +0100

    Link fix

--- src/components/TourAppBar.jsx
+++ src/components/TourAppBar.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -174,9 +173,7 @@ const TourAppBar = ({ title }) => {
             component="div"
             sx={{ display: { xs: 'none', sm: 'block' } }}
           >
-            <Link to='/' sx={{ textDecoration: 'none', color: 'primary-main' }}>
-              {title}
-            </Link>
+            {title}
           </Typography>
           <Search>
             <SearchIconWrapper>
