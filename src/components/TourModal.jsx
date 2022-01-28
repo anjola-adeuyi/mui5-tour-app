64da43d3dd0ce12a39eb5c91db81047be07cf767
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-01-28 09:47:05 +0100

    Date Range Picker and Modal added

--- src/components/TourModal.jsx
+++ src/components/TourModal.jsx
@@ -3,13 +3,13 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import StaticDateRangePickerDemo from './TourDateRangePicker'
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -31,12 +31,18 @@ export default function BasicModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+          <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ display: 'flex', alignItem: 'center', justifyContent: 'center' }}>
+            Pick a start date and an End date below
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <StaticDateRangePickerDemo />
+          <Box sx={{ mt: 2, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <Typography id="modal-modal-description">
+              Thanks for booking
+            </Typography>
+            
+            <Button variant='outlined' onClick={handleClose}>Book - Now</Button>
+        
+          </Box>
         </Box>
       </Modal>
     </div>
